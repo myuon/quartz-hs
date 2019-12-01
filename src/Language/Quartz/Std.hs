f892f33d97f3fa3c7d4f823bfc792cfc31bb86d8
myuon <ioi.joi.koi.loi@gmail.com>
2019-12-01 16:41:19 +0900

    feat(std): FizzBuzz example

--- src/Language/Quartz/Std.hs
+++ src/Language/Quartz/Std.hs
@@ -5,6 +5,7 @@ import Control.Error
 import qualified Data.Map as M
 import Data.Dynamic
 import Language.Quartz.AST
+import qualified Data.Primitive.Array as Array
 
 data FFIExceptions
   = InvalidExpr Dynamic
@@ -21,5 +22,25 @@ ffi = M.fromList
         _                 -> throwE $ InvalidExpr d
 
       return Unit
+    )
+  , ( Id ["range"]
+    , \[s, e] -> do
+      x <- (fromDynamic s :: Maybe Expr) ?? InvalidExpr s
+      y <- (fromDynamic e :: Maybe Expr) ?? InvalidExpr e
+      case (x, y) of
+        (Lit (IntLit x), Lit (IntLit y)) -> do
+          let arr = Array.fromList $ map (Lit . IntLit) [x .. y]
+          marr <- liftIO $ Array.thawArray arr 0 (Array.sizeofArray arr)
+          return $ Array $ MArray marr
+        _ -> throwE $ InvalidExpr s
+    )
+  , ( Id ["mod"]
+    , \[d1, d2] -> do
+      x <- (fromDynamic d1 :: Maybe Expr) ?? InvalidExpr d1
+      y <- (fromDynamic d2 :: Maybe Expr) ?? InvalidExpr d2
+      case (x, y) of
+        (Lit (IntLit x'), Lit (IntLit y')) ->
+          return $ Lit (IntLit (x' `mod` y'))
+        _ -> throwE $ InvalidExpr d1
     )
   ]
