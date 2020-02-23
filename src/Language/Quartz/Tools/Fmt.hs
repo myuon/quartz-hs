02ebef4c7d081b5374e98908d6dc7342d51da318
myuon <ioi.joi.koi.loi@gmail.com>
2020-02-23 15:04:24 +0900

    fix: token sequence in scope/block

--- src/Language/Quartz/Tools/Fmt.hs
+++ src/Language/Quartz/Tools/Fmt.hs
@@ -9,14 +9,15 @@ import Data.Text.Prettyprint.Doc
 import Language.Quartz.Tools.FmtLexer
 
 data Code
-  = Tokens (V.Vector Lexeme)
+  = AToken Lexeme
+  | Sequence (V.Vector Code)
   -- open punctuation close
   | Block String String String (V.Vector Code)
   | Scope String String String (V.Vector Code)
   deriving (Eq, Show)
 
 data StackElement s
-  = STokens (PBVector s Lexeme)
+  = STokens (PBVector s Code)
   | SCode Code
 
 parse :: [Lexeme] -> [Code]
@@ -33,29 +34,34 @@ parse tokens = runST $ do
       Lexeme _ (TSymbol t) | t == ")" -> close stack "(" "," ")"
       Lexeme _ (TSymbol t) | t == "<" -> start stack token
       Lexeme _ (TSymbol t) | t == ">" -> close stack "<" "," ">"
+      Lexeme _ (TSymbol t) | t == "{" -> start stack token
+      Lexeme _ (TSymbol t) | t == ";" -> continue stack
+      Lexeme _ (TSymbol t) | t == "}" -> close stack "{" ";" "}"
       _ -> do
         popped <- pop stack
         case popped of
           Just (STokens v) -> do
-            push v     token
+            push v     (AToken token)
             push stack (STokens v)
           Just (SCode c) -> do
             push stack (SCode c)
 
             v <- new 0
-            push v     token
+            push v     (AToken token)
             push stack (STokens v)
 
   vs <- fmap V.toList $ toVector stack
-  fmap (map fromJust . filter isJust) $ forM vs $ \v -> case v of
-    STokens p -> do
-      v <- toVector p
-      if V.null v then return Nothing else return (Just $ Tokens v)
-    SCode c -> return (Just c)
+  fmap (V.toList . V.concat . map fromJust . filter isJust)
+    $ forM vs
+    $ \v -> case v of
+        STokens p -> do
+          v <- toVector p
+          if V.null v then return Nothing else return (Just v)
+        SCode c -> return (Just $ V.singleton c)
  where
   start stack token = do
     v <- new 0
-    push v     token
+    push v     (AToken token)
     push stack (STokens v)
     w <- new 0
     push stack (STokens w)
@@ -65,19 +71,29 @@ parse tokens = runST $ do
     push stack (STokens v)
 
   close stack open punct close = do
-    vs <- popUntil stack (TSymbol open)
-    push stack (SCode $ Scope open punct close $ V.reverse $ V.fromList vs)
+    vs     <- popUntil stack (TSymbol open)
+    popped <- pop stack
+    case popped of
+      Just (STokens w) -> do
+        push w     (Scope open punct close $ V.reverse $ V.fromList vs)
+        push stack (STokens w)
 
   popUntil stack ch = do
     popped <- pop stack
     case popped of
       Just (STokens vs') -> do
         vs <- toVector vs'
-        if V.length vs == 1 && tokenOfLexeme (vs V.! 0) == ch
+        if isTokenOf ch vs
           then return []
-          else fmap (Tokens vs :) $ popUntil stack ch
+          else fmap (Sequence vs :) $ popUntil stack ch
       Just (SCode c) -> do
         fmap (c :) $ popUntil stack ch
+
+isTokenOf ch vs = if V.length vs == 1
+  then case (vs V.! 0) of
+    AToken t | tokenOfLexeme t == ch -> True
+    _ -> False
+  else False
 
 --formatDoc :: String -> Either String (Doc a)
 --formatDoc = pretty . alexScanTokens
--- test/Language/Quartz/Tools/SpecFmt.hs
+++ test/Language/Quartz/Tools/SpecFmt.hs
@@ -37,8 +37,8 @@ spec_fmt = do
       parse (alexScanTokens "(10)")
         `shouldBe` [
           Scope "(" "," ")" (V.fromList [
-            Tokens (V.fromList [
-              Lexeme (AlexPn 1 1 2) (Token "10")
+            Sequence (V.fromList [
+              AToken $ Lexeme (AlexPn 1 1 2) (Token "10")
             ])
           ])
         ]
@@ -47,17 +47,17 @@ spec_fmt = do
       parse (alexScanTokens "(10,20,30,40)")
         `shouldBe` [
           Scope "(" "," ")" (V.fromList [
-            Tokens (V.fromList [
-              Lexeme (AlexPn 1 1 2) (Token "10")
+            Sequence (V.fromList [
+              AToken $ Lexeme (AlexPn 1 1 2) (Token "10")
             ]),
-            Tokens (V.fromList [
-              Lexeme (AlexPn 4 1 5) (Token "20")
+            Sequence (V.fromList [
+              AToken $ Lexeme (AlexPn 4 1 5) (Token "20")
             ]),
-            Tokens (V.fromList [
-              Lexeme (AlexPn 7 1 8) (Token "30")
+            Sequence (V.fromList [
+              AToken $ Lexeme (AlexPn 7 1 8) (Token "30")
             ]),
-            Tokens (V.fromList [
-              Lexeme (AlexPn 10 1 11) (Token "40")
+            Sequence (V.fromList [
+              AToken $ Lexeme (AlexPn 10 1 11) (Token "40")
             ])
           ])
         ]
@@ -66,31 +66,57 @@ spec_fmt = do
       parse (alexScanTokens "(a: int, b: string, c: Hello<A,B,C>)")
         `shouldBe` [
           Scope "(" "," ")" (V.fromList [
-            Tokens (V.fromList [
-              Lexeme (AlexPn 1 1 2) (Token "a"),
-              Lexeme (AlexPn 2 1 3) (TSymbol ":"),
-              Lexeme (AlexPn 4 1 5) (Token "int")
+            Sequence (V.fromList [
+              AToken $ Lexeme (AlexPn 1 1 2) (Token "a"),
+              AToken $ Lexeme (AlexPn 2 1 3) (TSymbol ":"),
+              AToken $ Lexeme (AlexPn 4 1 5) (Token "int")
             ]),
-            Tokens (V.fromList [
-              Lexeme (AlexPn 9 1 10) (Token "b"),
-              Lexeme (AlexPn 10 1 11) (TSymbol ":"),
-              Lexeme (AlexPn 12 1 13) (Token "string")
+            Sequence (V.fromList [
+              AToken $ Lexeme (AlexPn 9 1 10) (Token "b"),
+              AToken $ Lexeme (AlexPn 10 1 11) (TSymbol ":"),
+              AToken $ Lexeme (AlexPn 12 1 13) (Token "string")
             ]),
-            Tokens (V.fromList [
-              Lexeme (AlexPn 20 1 21) (Token "c"),
-              Lexeme (AlexPn 21 1 22) (TSymbol ":"),
-              Lexeme (AlexPn 23 1 24) (Token "Hello")
-            ]),
-            Scope "<" "," ">" (V.fromList [
-              Tokens (V.fromList [
-                Lexeme (AlexPn 29 1 30) (Token "A")
-              ]),
-              Tokens (V.fromList [
-                Lexeme (AlexPn 31 1 32) (Token "B")
-              ]),
-              Tokens (V.fromList [
-                Lexeme (AlexPn 33 1 34) (Token "C")
+            Sequence (V.fromList [
+              AToken $ Lexeme (AlexPn 20 1 21) (Token "c"),
+              AToken $ Lexeme (AlexPn 21 1 22) (TSymbol ":"),
+              AToken $ Lexeme (AlexPn 23 1 24) (Token "Hello"),
+              Scope "<" "," ">" (V.fromList [
+                Sequence (V.fromList [
+                  AToken $ Lexeme (AlexPn 29 1 30) (Token "A")
+                ]),
+                Sequence (V.fromList [
+                  AToken $ Lexeme (AlexPn 31 1 32) (Token "B")
+                ]),
+                Sequence (V.fromList [
+                  AToken $ Lexeme (AlexPn 33 1 34) (Token "C")
+                ])
               ])
+            ])
+          ])
+        ]
+
+    it "" $ do
+      parse (alexScanTokens "{ println(\"foo\"); let a = 20; a * 2 }")
+        `shouldBe` [
+          Scope "{" ";" "}" (V.fromList [
+            Sequence (V.fromList [
+              AToken $ Lexeme (AlexPn 2 1 3) (Token "println"),
+              Scope "(" "," ")" (V.fromList [
+                Sequence (V.fromList [
+                  AToken $ Lexeme (AlexPn 10 1 11) (TStrLit "foo")
+                ])
+              ])
+            ]),
+            Sequence (V.fromList [
+              AToken $ Lexeme (AlexPn 18 1 19) (Token "let"),
+              AToken $ Lexeme (AlexPn 22 1 23) (Token "a"),
+              AToken $ Lexeme (AlexPn 24 1 25) (TSymbol "="),
+              AToken $ Lexeme (AlexPn 26 1 27) (Token "20")
+            ]),
+            Sequence (V.fromList [
+              AToken $ Lexeme (AlexPn 30 1 31) (Token "a"),
+              AToken $ Lexeme (AlexPn 32 1 33) (TSymbol "*"),
+              AToken $ Lexeme (AlexPn 34 1 35) (Token "2")
             ])
           ])
         ]
