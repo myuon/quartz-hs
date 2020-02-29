d4d2ea3ae2f0ab567dd729295c6f8617aee6f96b
myuon <ioi.joi.koi.loi@gmail.com>
2020-02-29 19:36:02 +0900

    fix: TokenConsumer monad

--- src/Language/Quartz/Parser.hs
+++ src/Language/Quartz/Parser.hs
@@ -1,20 +1,18 @@
 module Language.Quartz.Parser where
 
-import           Control.Applicative
+import           Control.Error
 import           Control.Monad.ST
 import           Control.Monad.State
+import           Control.Monad.Reader
 import qualified Data.Vector.PushBack          as PBV
 import           Language.Quartz.Lexer
 import           Language.Quartz.AST
 
-data StackElement s
-  = STokens (PBV.PBVector s (Expr AlexPosn))
-  | SCode (Expr AlexPosn)
+type TokenConsumer s e m a
+  = ExceptT String (ReaderT (PBV.PBVector s e) (StateT [Lexeme] m)) a
 
-type TokenConsumer m a = StateT [Lexeme] m a
-
-pop :: Monad m => TokenConsumer m (Maybe Lexeme)
-pop = do
+consume :: Monad m => TokenConsumer s (Expr AlexPosn) m (Maybe Lexeme)
+consume = do
   lexs <- get
   case lexs of
     (t : ts) -> do
@@ -22,56 +20,66 @@ pop = do
       return $ Just t
     _ -> return Nothing
 
-pushMaybe :: Monad m => Maybe Lexeme -> TokenConsumer m ()
-pushMaybe lex = case lex of
+prepare :: Monad m => Maybe Lexeme -> TokenConsumer s (Expr AlexPosn) m ()
+prepare lex = case lex of
   Just l -> do
     st <- get
     put $ l : st
   Nothing -> return ()
 
-expect :: Monad m => Token -> TokenConsumer m (Maybe Lexeme)
+expect :: Monad m => Token -> TokenConsumer s (Expr AlexPosn) m (Maybe Lexeme)
 expect t = do
-  lex <- pop
+  lex <- consume
   case fmap tokenOfLexeme lex of
     Just t' | t' == t -> return lex
-    _                 -> pushMaybe lex >> return Nothing
+    _                 -> prepare lex >> return Nothing
+
+report :: e -> TokenConsumer s e (ST s) ()
+report e = do
+  v <- ask
+  PBV.push v e
+
+pop :: TokenConsumer s e (ST s) (Maybe e)
+pop = do
+  v <- ask
+  PBV.pop v
 
 --
 
-literal :: TokenConsumer (ST s) (Maybe (Expr AlexPosn))
-literal = fmap (fmap Lit) $ do
-  lex <- pop
+exprShortTerminals
+  :: TokenConsumer s (Expr AlexPosn) (ST s) (Maybe (Expr AlexPosn))
+exprShortTerminals = do
+  lex <- consume
   case fmap tokenOfLexeme lex of
-    Just (TInt    n) -> return $ Just $ IntLit n
-    Just (TStrLit s) -> return $ Just $ StringLit s
-    _                -> pushMaybe lex >> return Nothing
+    Just (TInt    n) -> return $ Just $ Lit $ IntLit n
+    Just (TStrLit s) -> return $ Just $ Lit $ StringLit s
+    Just (TVar    n) -> return $ Just $ Var Nothing (Id [n])
+    Just TSelf       -> return $ Just $ Self SelfType
+    _                -> return Nothing
 
-var :: TokenConsumer (ST s) (Maybe (Expr AlexPosn))
-var = do
-  lex <- pop
-  case fmap tokenOfLexeme lex of
-    Just (TVar n) -> return $ Just $ Var Nothing (Id [n])
-    Just TSelf    -> return $ Just $ Self SelfType
-    Nothing       -> return Nothing
-    _             -> pushMaybe lex >> return Nothing
-
-exprShort :: TokenConsumer (ST s) (Maybe (Expr AlexPosn))
+exprShort :: TokenConsumer s (Expr AlexPosn) (ST s) ()
 exprShort = do
-  lit <- literal
-  v   <- var
-
-  -- 遅延評価なので最初にJustになったタイミングでそれ以降のアクションは実行されない
-  return $ lit <|> v
+  e1 <- exprShortTerminals
+  forM_ e1 report
 
 parserExpr :: [Lexeme] -> Either String (Expr AlexPosn)
-parserExpr lexs =
-  let (a, rest) = runST $ flip runStateT lexs $ do
-        exprShort
-  in  if not (null rest)
-        then Left ("parse failed: " ++ show rest)
-        else case a of
-          Nothing -> Left "rule not found"
-          Just a  -> return a
+parserExpr lexs = runST $ do
+  stack          <- PBV.new 0
+  (result, rest) <-
+    flip runStateT lexs $ flip runReaderT stack $ runExceptT $ do
+      exprShort
+
+  unless (null rest) $ fail $ "Parse Error (tokens): " ++ show rest
+
+  len <- PBV.length stack
+  when (len /= 1) $ do
+    rs <- PBV.toList stack
+    fail $ "Parse Error (stack elements): " ++ show rs
+
+  Just k <- PBV.pop stack
+  return $ do
+    result
+    return k
 
 parser :: [Lexeme] -> Either String (Decl AlexPosn)
 parser = undefined
